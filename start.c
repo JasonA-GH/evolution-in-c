19942879b7adab0e2d35a16fa4c71ef7b09ae379
JasonA-GH <136386469+JasonA-GH@users.noreply.github.com>
2025-02-04 20:20:48 -0500

    Better evolution

--- start.c
+++ start.c
@@ -13,9 +13,9 @@ typedef struct agent_struct
 {
   int x;
   int y;
-  int dir;
+  int* dirs;
   double reward;
-  int seed;
+  //int seed;
 }agent;
 
 agent* agents;
@@ -23,13 +23,13 @@ agent* agents;
 
 void do_agent(agent* a)
 {
-  srand(a->seed);
+  //srand(a->seed);
   a->x = 0;
   a->y = 0;
   for(int i=0; i < AGENT_STEPS; i++)
     {
-      a->dir = rand()%4;
-      switch(a->dir)
+      //a->dir = rand()%4;
+      switch(a->dirs[i])
 	{
 	case 0:
 	  {
@@ -56,6 +56,40 @@ void do_agent(agent* a)
     }
 }
 
+void copy_dirs_from(agent* base, agent* end)
+{
+  for(int i=0; i < AGENT_STEPS; i++)
+    {
+      end->dirs[i] = base->dirs[i];
+    }
+}
+
+void copy_dirs_from_start_end(agent* base, agent* target, int start, int end)
+{
+  for(int i=start; i < end; i++)
+    {
+      target->dirs[i] = base->dirs[i];
+    }
+}
+
+void copy(agent* a, agent* b)
+{
+  //b->x = a->x;
+  //b->y = a->y;
+  b->reward = a->reward;
+  copy_dirs_from(a, b);
+  //b->seed = a->seed;
+}
+
+void init_dirs(agent* a)
+{
+  a->dirs = malloc(sizeof(int)*AGENT_STEPS);
+  for(int i=0; i < AGENT_STEPS; i++)
+    {
+      a->dirs[i] = rand()%4;
+    }
+}
+
 void init_agents(int count)
 {
   //IF THIS IS CALLED MORE THAN ONCE IT WILL LEAK MEMORY
@@ -65,25 +99,48 @@ void init_agents(int count)
     {
       agents[i].x = 0;
       agents[i].y = 0;
-      agents[i].dir = 0;
+      //agents[i].dir = 0;
+      init_dirs(&(agents[i]));
       agents[i].reward = 0;
-      agents[i].seed = rand();
+      //agents[i].seed = rand();
     }
 }
 
-void evolve(agent* a)
+void evolve(agent* base, agent* better)
 {
   //srand(time(NULL));
   //a->seed++;
-  
-}
 
-void copy(agent* a, agent* b)
-{
-  b->x = a->x;
-  b->y = a->y;
-  b->reward = a->reward;
-  b->seed = a->seed;
+
+  //Randomly choose index
+  //Randomly choose whether to change first or second half
+  //10% chance to mutate 10% of the dirs
+
+  int index = rand()%AGENT_STEPS;
+  if(rand()%2 == 0)//FIRST HALF
+    {
+      //end = AGENT_STEPS;
+      copy_dirs_from_start_end(better, base, 0, index);
+    }
+  else//SECOND HALF
+    {
+      copy_dirs_from_start_end(better, base, index, AGENT_STEPS);
+    }
+
+  if(rand()%10 == 0)
+    {
+      int count = rand()%(AGENT_STEPS/10);
+      for(int i=0; i < count; i++)
+	{
+	  base->dirs[i] = rand()%4;
+	}
+    }
+  
+  //bottom half (minus the bottom 10)
+  //Convert entire ai to Best
+  //Mutate 10% of dirs
+
+  //Last 10% just randomly generate
 }
 
 void sort()
@@ -97,6 +154,7 @@ void sort()
 	  if(agents[i+1].reward > agents[i].reward)
 	    {
 	      agent temp;
+	      init_dirs(&temp);
 	      copy(&(agents[i+1]), &temp);
 	      copy(&(agents[i]), &(agents[i+1]));
 	      copy(&temp, &(agents[i]));
@@ -138,18 +196,19 @@ int main()
 	  get_reward(&(agents[i]));
 	}
 
+
       sort();
 
       if(z%100 == 0)
-	printf("%d | Best: %lf x: %d y: %d seed: %d| Worst: %lf x: %d y: %d seed: %d\n", z, agents[0].reward, agents[0].x, agents[0].y, agents[0].seed, agents[agent_count-1].reward, agents[agent_count-1].x, agents[agent_count-1].y, agents[agent_count-1].seed);
+	printf("%d | Best: %lf x: %d y: %d | Worst: %lf x: %d y: %d\n", z, agents[0].reward, agents[0].x, agents[0].y, agents[agent_count-1].reward, agents[agent_count-1].x, agents[agent_count-1].y);
 
       //printf("best seed %d\n", agents[0].seed);
       for(int i=1; i < agent_count; i++)
 	{
-	  evolve(&(agents[i]));
+	  evolve(&(agents[i]), &(agents[i-1]));
 	}
     }
-  printf("%d | Best: %lf x: %d y: %d seed: %d| Worst: %lf x: %d y: %d seed: %d\n", z, agents[0].reward, agents[0].x, agents[0].y, agents[0].seed, agents[agent_count-1].reward, agents[agent_count-1].x, agents[agent_count-1].y, agents[agent_count-1].seed);
+  printf("%d | Best: %lf x: %d y: %d | Worst: %lf x: %d y: %d\n", z, agents[0].reward, agents[0].x, agents[0].y, agents[agent_count-1].reward, agents[agent_count-1].x, agents[agent_count-1].y);
 
   
   
